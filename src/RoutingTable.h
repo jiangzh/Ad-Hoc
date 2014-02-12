4cccb87eb560fd3f12739ab7e2b9a4e9a4dcc665
Karim Chahal <10008641@e008m07.istic.univ-rennes1.fr>
2014-02-12 16:47:17 +0100

    Dernière version du Mercredi 12/02 : RoutingTable.h

--- src/RoutingTable.h
+++ src/RoutingTable.h
@@ -11,25 +11,39 @@
 #ifndef ROUTINGTABLE_H_
 #define ROUTINGTABLE_H_
 #include "Route.h"
-#
-
+#include "Node.h"
+#include <stdlib.h>
+#include <list>
+#include <stdio.h>
 
 class RoutingTable{
 
 public:
 	RoutingTable(); // Initializes the Application Routing Table.
-	void addRoute(Route r); // add a new route in the Routing Table of the application
-	void deleteRoute(Route r); // delete an existing route from the Routing Table of the application.
-	void updaterouteList();
-	void systemTableUpdate(std::list<Route> routeList);
+
+	void systemTableUpdate(Node *noeud);
 
 	inline std::list<Route> getRouteList(){ // returns the private attribute routeList containing all the routes
-			return this->routeList;
+			return mRouteList;
 		}
 
 private:
-	std::list<Route> routeList;
+	std::list<Route> mRouteList;
 
+	/**
+	 *  Function addRoute
+	 *
+	 *  	add a new route in the Routing Table of the application
+	 *  @param Route r
+	 */
+	void addRoute(Route *r); /** add a new route in the Routing Table of the application. */
+
+	/**
+	 *  Funcion delete route
+	 *  	delete an existing route from the Routing table of the application
+	 */
+	void deleteRoute(Route *r); /** delete an existing route from the Routing Table of the application. */
+	void updateRoute(Route *r); /** replace an existing by r if route r is better than the existing one. */
 };
 
 
