5c6bf7a42d410824cb58334fec817b36ef08f857
celine <serinu@hotmail.fr>
2014-02-12 19:09:19 +0100

    dernières modif headers.h pour avoir les mêmes noms que le groupe Message

--- src/headers.h
+++ src/headers.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <stdint.h>
 #include <netinet/in.h>
 #include <iostream>
@@ -11,7 +14,7 @@ typedef struct {
 }packetHeader;
 
 /* entete generique du packet */
-typedef struct{
+typedef struct {
   uint8_t messageType;
   uint8_t vTime;
   uint16_t messageSize;
@@ -19,31 +22,37 @@ typedef struct{
   uint8_t timeToLive;
   uint8_t hopCount;
   uint16_t messageSequenceNumber;
-}messageHeader;
+} messageHeader;
 
-typedef struct{
+typedef struct {
   uint8_t linkCode;
   uint8_t reserved;
   uint16_t linkMessageSize;
   in6_addr* neighborsAddrList;// array of neighbors (ipv6)
   int sizeNAL;// size of the array neighborsAddrList
-}helloNeighborList;
+} helloNeighborList;
 
 /* entete de message hello */
-typedef struct{
-  uint16_t reserved1;
+typedef struct {
+  uint16_t reserved;
   uint8_t hTime;
   uint8_t willingness;
   helloNeighborList* neighbors;// array of the neighbors (1 helloNeighborList by interface)
   int sizeNL; // size of the array neighbors
-}helloMessageHeader;
+} helloMessageHeader;
 /* link message */
 
 /* entete de message */
-
-typedef struct{
+typedef struct {
   uint16_t ANSN;
   uint16_t reserved;
   in6_addr* advertisedNeighborMainAddress ;// tableau d'IPV6ADDR
   int sizeANMA;// size of the arrray advertisedNeighborMainAddress
 }tcMessageHeader;
+
+typedef union {
+  tcMessageHeader* bodyTC;
+  helloMessageHeader* bodyHello;
+} msgBody;
+
+#endif
